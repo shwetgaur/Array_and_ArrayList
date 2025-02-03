d94bc1f81f7e095b80ceb02ef0b3e4884dcc5fc1
shwetgaur <146635341+shwetgaur@users.noreply.github.com>
2025-02-03 22:31:33 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,18 @@
+package ArrayFunctions;
+import java.util.Scanner;
+
+public class UserInput {
+    public int[] arrayInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        return arr;
+    }
+}
