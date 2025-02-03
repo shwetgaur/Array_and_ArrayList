3dccc7eb7f8b577540fa6f0f2adeffd0aba681da
shwetgaur <146635341+shwetgaur@users.noreply.github.com>
2025-02-03 22:32:04 +0530

    Function FIle

---
+++ ArrayFunctions.java
@@ -0,0 +1,52 @@
+package ArrayFunctions;
+import java.util.ArrayList;
+
+public class ArrayFunctions {
+    public void DisplayArray(ArrayList<Integer> arr) {
+        System.out.println("Array elements are: " + arr);
+    }
+
+    public void Distribute() {
+        UserInput in = new UserInput();
+        int[] arr = in.arrayInput();
+        ArrayList<Integer> evenList = new ArrayList<>();
+        ArrayList<Integer> oddList = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                evenList.add(arr[i]);
+            } else {
+                oddList.add(arr[i]);
+            }
+        }
+
+        DisplayArray(evenList);
+        DisplayArray(oddList);
+    }
+
+    public int Difference() {
+        UserInput in = new UserInput();
+        int[] arr = in.arrayInput();
+        ArrayList<Integer> al = new ArrayList<>();
+        int minDiff = Integer.MAX_VALUE;
+        int minIndex = -1;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int diff = Math.abs(arr[i] - arr[i + 1]);
+            al.add(diff);
+            if (diff < minDiff) {
+                minDiff = diff;
+                minIndex = i;
+            }
+        }
+        DisplayArray(al);
+        return minIndex;
+    }
+
+    public static void main(String[] args) {
+        ArrayFunctions af = new ArrayFunctions();
+        int minIndex = af.Difference();
+        System.out.println("Index of the pair with minimum difference: " + minIndex);
+        af.Distribute();
+    }
+}
